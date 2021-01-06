1e5da3eee3ca1f3c1998eadf99f648409837d528
Steve Kinney <hello@stevekinney.net>
2021-01-05 21:04:19 -0700

    Get the simplified boilerplate in place

---
+++ src/Application.js
@@ -0,0 +1,11 @@
+const Application = () => {
+  return (
+    <div className="Application">
+      <header className="Application-header">
+        <h1>Taskmaster</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Application;
